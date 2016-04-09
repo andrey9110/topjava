6bee5fcff06b24df70e7d14eb66f2683121d513a
andrey9110 <titarenko9110@mail.ru>
2016-04-09 12:18:26 +0300

    4-HW5-Optional-fix-servlet-main

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
@@ -1,6 +1,11 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.UserMeal;
@@ -9,13 +14,15 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 abstract public class AbstractUserMealServiceTest extends AbstractServiceTest {
-    private static final Logger LOG = LoggerFactory.getLogger(UserMealServiceTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractUserMealServiceTest.class);
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
